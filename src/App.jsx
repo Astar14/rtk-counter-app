9dc0a7ae9986e798cfc7e3fc2eee0aa7d223df76
Astar14 <tamit8587@gmail.com>
2025-01-20 16:16:16 +0530

    Added the functionality successfull increment decrement  reset

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 
+import { useDispatch, useSelector } from 'react-redux'
 import './App.css'
+import { decrement, increment, reset } from './store/slices/counter'
 
 function App() {
 
+  const count = useSelector((state) => state.counter.count)
+  const dispatch = useDispatch()
+
   return (
     <>
-     
+      <h1>Counter: {count}</h1>
+      <button onClick={()=>dispatch(increment())}>Increment</button>
+       <button onClick={()=>dispatch(decrement())}>Decrement</button>
+      <button onClick={()=>dispatch(reset())}>Reset</button> 
     </>
   )
 }
--- src/store/slices/counter/index.js
+++ src/store/slices/counter/index.js
@@ -8,9 +8,9 @@ export const counterSlice = createSlice({
   name: "counter",
   initialState,
   reducers: {
-    increment: (state) => {state.count + 1},
-    decrement: (state) => { state.count - 1 },
-    reset:(state) => {state.count = count}
+    increment: (state) => {state.count += 1},
+    decrement: (state) => { state.count -= 1 },
+    reset:(state) => {state.count = 0}
   },
 });
 
